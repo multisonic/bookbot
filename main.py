46e424dbf6668e4fb29f65878210a60983f22eff
multisonic <me@mikezaleski.com>
2024-03-05 13:23:32 -0500

    my initial function to count words in a book

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    words = count_words(text)
     print(text)
+    print("words", words)
 
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
+    
 main()
