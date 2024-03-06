87b203ef24bb6902a8e23005ef38cf12eef07729
multisonic <me@mikezaleski.com>
2024-03-06 10:35:15 -0500

    16. Print a Report - my solution

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    # print(get_chars_dict(text))
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(count_letters(text))
+    print("")
+    print(characters_found(get_chars_dict(text)))
+    print(f"--- End report ---")
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
-
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
     
 def get_chars_dict(text):
     chars = {}
@@ -25,5 +24,24 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
+def characters_found(dict):
+    list = []
+    for letter in dict:
+      if letter.isalpha():
+        new_dict = {}
+        count = dict[letter]
+        new_dict["letter"] = letter
+        new_dict["count"] = count
+        list.append(new_dict)
+    def sort_on(letter_dictionary):
+      return letter_dictionary["count"]
+    list.sort(reverse=True, key=sort_on)
+    for letter in list:
+        print(f"The '{letter["letter"]}' was found {letter["count"]} times")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
