c1c8c3c27347205e273db7c0b0eb48b941aa11df
multisonic <me@mikezaleski.com>
2024-03-06 09:58:26 -0500

    15. Count Letters - My Solution

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print(count_letters(text))
 
 
 def get_num_words(text):
@@ -13,6 +14,13 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def count_letters(text):
+    lowercase_input = text.lower()
+    freq = {}
+    for c in set(lowercase_input):
+       freq[c] = lowercase_input.count(c)
+    return freq
 
 
 main()
